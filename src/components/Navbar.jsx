f4c4b8256eb2c252795ddc6faf55a3e024a432a0
DanteThuta <dantethuta97@gmail.com>
2022-03-16 17:53:30 +0630

    Creating NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function Navbar() {
+  const Container = styled.div`
+    background-color: black;
+    height: 60px;
+  `;
+  return <Container>Nav</Container>;
+}
+
+export default Navbar;
