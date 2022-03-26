30f84ba254f0464eb72d035cc922251bb8c001ea
Dante <dante@Dante-MacBook-Air.local>
2022-03-26 20:48:09 +0630

    Footer Section (Incomplete)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import {
   Instagram,
   MailOutline,
   Phone,
+  Room,
 } from "@material-ui/icons";
 
 const Container = styled.div`
@@ -53,10 +54,51 @@ const Center = styled.div`
 
 const CTitle = styled.h1`
   font-size: 40px;
+  padding: 15px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+  font-size: 20px;
+
+  width: 50%;
+  margin-bottom: 5px;
+
+  cursor: pointer;
 `;
 
 // Right Section
-const Right = styled.div``;
+const Right = styled.div`
+  flex: 1;
+`;
+
+const RTitle = styled.h1`
+  font-size: 40px;
+  padding: 15px;
+`;
+
+const ContactItem = styled.div`
+  /* padding: 10px; */
+  margin: 10px;
+
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  padding-right: 30px;
+`;
+
+const Payment = styled.div`
+  width: 50%;
+`;
 
 const Footer = () => {
   return (
@@ -87,9 +129,43 @@ const Footer = () => {
         </Socialbar>
       </Left>
       <Center>
-        <CTitle>Hello</CTitle>
+        <CTitle>Useful Links</CTitle>
+
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
       </Center>
-      <Right></Right>
+      <Right>
+        <RTitle>Contact</RTitle>
+        <ContactItem>
+          <Icon>
+            <Room />
+          </Icon>
+          622 Dixie Path , South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <Icon>
+            <Phone />
+          </Icon>
+          +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <Icon>
+            <MailOutline />
+          </Icon>
+          contact@dante.
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
